f23aa14f0d6c368ed1deb6521e3641e036ed33b0
leech <fairprice81000@gmail.com>
2022-06-03 21:49:05 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, IconButton, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  Header,
+  MainContainer,
+  InputContainer,
+  ResultContainer,
+  ResultURL,
+  ButtonContainer,
+} from "./Style";
+import { APIKEY } from "./api/api"; //set APIKEY
+import Snackbar from "@mui/material/Snackbar";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 function App() {
+  const [OriginalUrl, setOriginalUrl] = useState("");
+  const [ShortenURL, setShortenURL] = useState("");
+  const [Shorten, setShorten] = useState(false);
+  const [isSnackbar, setSnackbar] = useState(false);
+  const handleClick = async () => {
+    if (OriginalUrl === "") {
+      alert("請輸入");
+      return;
+    }
+    setShorten(false);
+    FetchURL(OriginalUrl);
+  };
+  const FetchURL = async (Url) => {
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        apikey: APIKEY,
+      },
+      body: JSON.stringify({
+        destination: Url,
+        domain: { fullName: "st.chihsuan-lee.dev" },
+      }),
+    };
+
+    await fetch("https://api.rebrandly.com/v1/links", options)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.shortUrl) {
+          setShortenURL(response.shortUrl);
+          setShorten(true);
+          clearInput();
+        } else {
+          alert("Invalid format");
+          clearInput();
+        }
+      });
+  };
+  const clearInput = () => {
+    setOriginalUrl("");
+  };
+  const handleCopy = () => {
+    navigator.clipboard.writeText("https://" + ShortenURL);
+    setSnackbar(true);
+  };
+  const handleSnackbarClose = () => {
+    setSnackbar(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <div>
+        <Header>
+          <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+            Short url
+          </Typography>
+        </Header>
+        <InputContainer>
+          <TextField
+            label="Original URL"
+            variant="standard"
+            fullWidth
+            onChange={(e) => {
+              setOriginalUrl(e.target.value);
+            }}
+          />
+          <Button
+            variant="contained"
+            onClick={handleClick}
+            disabled={OriginalUrl ? false : true}
+          >
+            shorten
+          </Button>
+        </InputContainer>
+        {Shorten && ShortenURL && (
+          <ResultContainer>
+            <ResultURL>
+              <Typography
+                variant="subtitle2"
+                sx={{ fontFamily: "Roboto Mono" }}
+              >
+                <a
+                  href={`https://${ShortenURL}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  {ShortenURL}
+                </a>
+              </Typography>
+            </ResultURL>
+            <ButtonContainer>
+              <IconButton>
+                <ContentCopyIcon onClick={handleCopy} />
+              </IconButton>
+
+              <IconButton>
+                <a
+                  style={{ textDecoration: "none", color: "gray" }}
+                  href={`https://twitter.com/intent/tweet?text=https://${ShortenURL}`}
+                >
+                  <TwitterIcon />
+                </a>
+              </IconButton>
+            </ButtonContainer>
+          </ResultContainer>
+        )}
+        <Snackbar
+          open={isSnackbar}
+          autoHideDuration={5000}
+          onClose={handleSnackbarClose}
+          message="The shortened URL has been copied to your clipboard"
+        />
+      </div>
+    </MainContainer>
   );
 }
 
---
+++ src/Style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+`;
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90vw;
+`;
+export const ResultContainer = styled.div`
+  margin-top: 1vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 90vw;
+`;
+export const ResultURL = styled.div`
+  border: 1px solid;
+  border-color: #696969;
+  background-color: gray;
+  padding: 0.2rem; ;
+`;
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
